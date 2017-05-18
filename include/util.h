620159ef66313e55de8f053ed1ba4c4c447396be
mateusm <mateus.mesturini@gmail.com>
2017-05-18 01:12:46 -0300

    funcoes de utilidade colocadas em util.cpp

---
+++ include/util.h
@@ -0,0 +1,20 @@
+#ifndef __HUFFMANCODING_H_INCLUDED__
+#define __HUFFMANCODING_H_INCLUDED__
+
+#include "../include/btree.h"
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+typedef unordered_map<char, boost::dynamic_bitset<>> char_bit_t;
+typedef unordered_map<char, double> char_doub_t;
+
+void generateHuffmanTree(vector<BTree *> &fila);
+BTree *getLowestProb(vector<BTree *> &fila);
+void saveProbs(char_doub_t map);
+void exportOriginalToBinFile(string str);
+void exportCodedToBinFile(string str, char_bit_t codes);
+char_bit_t calcCodMap(char_doub_t chars, BTree *tree);
+
+#endif
--- src/huffman-coding.cpp
+++ src/huffman-coding.cpp
@@ -1,4 +1,4 @@
-#include "../include/huffman-coding.h"
+#include "../include/util.h"
 #include "../include/btree.h"
 #include "../include/compress.h"
 #include "../include/decompress.h"
@@ -23,7 +23,6 @@ int main(int argc, char *argv[]) {
 
 
   char_doub_t probOfChars;
-
   calcProb(probOfChars, entrada);
   // printMap(probOfChars);
 
@@ -50,105 +49,4 @@ int main(int argc, char *argv[]) {
   // decompress(charCodMap['F'], "probs.txt");
 
   return 0;
-}
-
-void generateHuffmanTree(vector<BTree *> &fila) {
-
-  while (fila.size() > 1) {
-
-    BTree *folha1 = getLowestProb(fila);
-    BTree *folha2 = getLowestProb(fila);
-
-    // cria novo node com com a soma das duas menores probabilidades
-    // e os adicionam como filhos
-    BTree *newNode = new BTree(folha1, folha2);
-
-    // adiciona nova probabilidade na fila
-    fila.push_back(newNode);
-  }
-
-  return;
-}
-
-// retorna folha com menor probabilidade e
-// já a deleta da fila!
-BTree *getLowestProb(vector<BTree *> &fila) {
-
-  double menorProb = fila.front()->getProb();
-  BTree *folhaMenorProb = fila.front();
-
-  // busca node com menor probabilidade
-  for (auto i : fila) {
-    if (i->getProb() < menorProb) {
-      menorProb = i->getProb();
-      folhaMenorProb = i;
-    }
-  }
-
-  // deleta o respectivo node da fila
-  // esse for feio é porque preciso pegar o indice do vetor
-  for (vector<BTree *>::size_type i = 0; i != fila.size(); ++i) {
-    if (fila[i]->getProb() == folhaMenorProb->getProb() &&
-        fila[i]->getChar() == folhaMenorProb->getChar()) {
-
-      fila.erase(fila.begin() + i);
-
-      return folhaMenorProb;
-    }
-  }
-  return folhaMenorProb;
-}
-
-void saveProbs(char_doub_t map) {
-
-  ofstream outStream;
-  string fileName("output/probabilidades.txt");
-
-  outStream.open(fileName);
-
-  vector<double> values;
-  for (auto n : map)
-    values.push_back(n.second);
-
-  sort(values.begin(), values.end(), std::greater<int>());
-
-  for (auto it : values) {
-    for (auto iter : map) {
-      if (iter.second == it) {
-        outStream << iter.first << "\t" << iter.second << "\n";
-        map.erase(iter.first);
-        break;
-      }
-    }
-  }
-
-  outStream.close();
-  cout << "file \"" + fileName + "\" saved\n";
-
-  return;
-}
-
-void exportOriginalToBinFile(string str) {
-  ofstream outfile("output/textoOriginal.bin", ofstream::binary);
-  for (unsigned i = 0; i < str.length(); i++) {
-    outfile << std::bitset<8>((int)str[i]);
-  }
-
-  cout << "original message saved in binary. \n";
-}
-
-void exportCodedToBinFile(string str, char_bit_t codes) {
-  string file = "output/textoCodificado.bin";
-
-  ofstream myFile(file, ios::out | ios::binary);
-
-  for (auto i : str) {
-    myFile << codes[i];
-  }
-
-  myFile.close();
-
-  cout << "coded message saved in binary. \n";
-
-  return;
 }
