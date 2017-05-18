2bf9cca254eb3d2a0ecb42e58941cd6692949c08
mateusm <mateus.mesturini@gmail.com>
2017-05-18 01:45:05 -0300

    em command line: exe + 1 argumento = compressao /// exe + 2 argumentos = descompressao

--- src/compress.cpp
+++ src/compress.cpp
@@ -1,5 +1,48 @@
+#include "../include/util.h"
+#include "../include/btree.h"
 #include "../include/compress.h"
+#include "../include/decompress.h"
+#include "../include/prob-Calc.h"
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <bitset>
+#include <boost/dynamic_bitset.hpp>
+#include <fstream>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
 
 typedef std::unordered_map<char, boost::dynamic_bitset<>> char_bit_t;
+
+void huffmanCompress(string entrada){
+
+
+
+    char_doub_t probOfChars;
+    calcProb(probOfChars, entrada);
+    // printMap(probOfChars);
+
+    saveProbs(probOfChars);
+
+    vector<BTree *> filaDePrioridades;
+    for (auto n : probOfChars) {
+      BTree *folha = new BTree(n.second, n.first);
+      filaDePrioridades.push_back(folha);
+    }
+
+    // ALGORITMO PARA GERAR A ÁRVORE
+    generateHuffmanTree(filaDePrioridades);
+
+    BTree *huffmanTree = filaDePrioridades[0];
+    // huffmanTree->printTree(huffmanTree->getRoot());
+
+    char_bit_t charCodMap;
+    charCodMap = calcCodMap(probOfChars, huffmanTree);
+
+    exportOriginalToBinFile(entrada);
+    exportCodedToBinFile(entrada, charCodMap);
+
+}
--- src/huffman-coding.cpp
+++ src/huffman-coding.cpp
@@ -18,35 +18,26 @@ typedef unordered_map<char, double> char_doub_t;
 
 int main(int argc, char *argv[]) {
 
-//Lê arquivo de entrada por linha de comando
-  string entrada = argv[1];
+  string entrada;
+  string entradaComprimida;
+  string arquivoProbabilidades;
 
+    switch(argc){
 
-  char_doub_t probOfChars;
-  calcProb(probOfChars, entrada);
-  // printMap(probOfChars);
+    case 2:   entrada = argv[1];
+              huffmanCompress(entrada);
+              break;
 
-  saveProbs(probOfChars);
+    case 3:   entradaComprimida = argv[1];
+              arquivoProbabilidades = argv[2];
+              //huffmanDecompress(entradaComprimida, arquivoProbabilidades);
+              break;
 
-  vector<BTree *> filaDePrioridades;
-  for (auto n : probOfChars) {
-    BTree *folha = new BTree(n.second, n.first);
-    filaDePrioridades.push_back(folha);
+    default: cout << "ENTRADA INVALIDA" << endl;
   }
 
-  // ALGORITMO PARA GERAR A ÁRVORE
-  generateHuffmanTree(filaDePrioridades);
 
-  BTree *huffmanTree = filaDePrioridades[0];
-  // huffmanTree->printTree(huffmanTree->getRoot());
 
-  char_bit_t charCodMap;
-  charCodMap = calcCodMap(probOfChars, huffmanTree);
-
-  exportOriginalToBinFile(entrada);
-  exportCodedToBinFile(entrada, charCodMap);
-
-  // decompress(charCodMap['F'], "probs.txt");
 
   return 0;
 }
